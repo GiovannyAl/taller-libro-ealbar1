aaa53c99434538fe22c3871274ca69c28fa2bf44
GiovannyAl <137365473+GiovannyAl@users.noreply.github.com>
2024-09-20 16:22:21 -0500

    primera version

--- src/Libro.java
+++ src/Libro.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Libro {
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+    private int anoPublicacion;
+
+    public Libro(String titulo, String autor, int numeroPaginas, int anoPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+        this.anoPublicacion = anoPublicacion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public int getAnoPublicacion() {
+        return anoPublicacion;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public void setAnoPublicacion(int AnoPublicacion) {
+        this.anoPublicacion = anoPublicacion;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Libro librouno = new Libro("El principito", "Antoine de Saint-Exupéry", 96, 1943);
+        Libro librodos = new Libro("Cien años de soledad", "Gabriel García Márquez", 417, 1967);
+
+        System.out.println("Libro 1:");
+        System.out.println("Titulo: " + librouno.getTitulo());
+        System.out.println("Autor: " + librouno.getAutor());
+        System.out.println("Numero de paginas: " + librouno.getNumeroPaginas());
+        System.out.println("Año de publicación: " + librouno.getAnoPublicacion());
+
+        System.out.println("\nLibro 2:");
+        System.out.println("Titulo: " + librodos.getTitulo());
+        System.out.println("Autor: " + librodos.getAutor());
+        System.out.println("Numero de paginas: " + librodos.getNumeroPaginas());
+        System.out.println("Año de publicación: " + librodos.getAnoPublicacion());
+
+        librouno.setNumeroPaginas(100);
+
+        System.out.println("\nLibro 1 actualizado:");
+        System.out.println("Titulo: " + librouno.getTitulo());
+        System.out.println("Autor: " + librouno.getAutor());
+        System.out.println("Numero de paginas: " + librouno.getNumeroPaginas());
+        System.out.println("Año de publicación: " + librouno.getAnoPublicacion());
+
     }
 }
